9d75491f3ea98395a7e29fe8a088749c4f649df9
thruway22 <121792615+thruway22@users.noreply.github.com>
2023-02-01 05:39:15 +0300

    Update app.py

--- app.py
+++ app.py
@@ -19,17 +19,18 @@ def display_input_widgets2(stride):
 st.title('NextTrade')
 csv_file = st.file_uploader('Upload a file', type='CSV')
 
-with st.expander('or manually input'):
-    input_form = st.form('input_form')
-    if csv_file is None:
+if csv_file is None:
+    with st.expander('or manually input'):
+        input_form = st.form('input_form')
         csv_string_placeholder = 'VTI,14,65\nBND,5,15\nKSA,3,20'
         csv_string = input_form.text_area('csv_string', height=150, placeholder=csv_string_placeholder, label_visibility='collapsed')
-    else:
-        df = pd.read_csv(csv_file, names=['ticker', 'current_shares', 'target_weight'])
-        df = df.set_index('ticker')
-        csv_string_value = df.to_csv(header=False)
-        csv_string = input_form.text_area('csv_string', height=150, value=csv_string_value, label_visibility='collapsed')
-    input_form_submitted = input_form.form_submit_button("Submit")
+        input_form_submitted = input_form.form_submit_button("Submit")
+        
+else:
+    df = pd.read_csv(csv_file, names=['ticker', 'current_shares', 'target_weight'])
+    df = df.set_index('ticker')
+    st.table(df)
+    
 
 
 
