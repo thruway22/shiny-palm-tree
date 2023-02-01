42dfb902e9b2bf41761003a397eb3acad02c9196
thruway22 <121792615+thruway22@users.noreply.github.com>
2023-02-01 05:19:13 +0300

    Update app.py

--- app.py
+++ app.py
@@ -7,7 +7,14 @@ def display_input_widgets(df, stride):
     locals()['ticker%s' % stride] = locals()['col%s0' % stride].text_input('ticker%s' % stride, value=df.index[stride], label_visibility='collapsed')
     locals()['share%s' % stride] = locals()['col%s1' % stride].number_input('share%s' % stride, value=df['current_shares'][stride], min_value=0, step=1, format='%d', label_visibility='collapsed')
     locals()['target%s' % stride] = locals()['col%s2' % stride].number_input('target%s' % stride, value=df['target_weight'][stride], min_value=0.0, step=0.1, format='%.1f', label_visibility='collapsed')
-    globals().update(locals())  
+    globals().update(locals()) 
+    
+def display_input_widgets2(stride):
+    locals()['col%s0' % stride], locals()['col%s1' % stride], locals()['col%s2' % stride] = form.columns(3)
+    locals()['ticker%s' % stride] = locals()['col%s0' % stride].text_input('ticker%s' % stride, label_visibility='collapsed')
+    locals()['share%s' % stride] = locals()['col%s1' % stride].number_input('share%s' % stride, min_value=0, step=1, format='%d', label_visibility='collapsed')
+    locals()['target%s' % stride] = locals()['col%s2' % stride].number_input('target%s' % stride, min_value=0.0, step=0.1, format='%.1f', label_visibility='collapsed')
+    globals().update(locals())
 
 st.title('NextTrade')
 uploaded_file = st.file_uploader('Choose a file', type='CSV')
@@ -41,8 +48,14 @@ if csv_form_submitted:
     cola.write('Ticker')
     colb.write('Current Shares (x)')
     colc.write('Target Weight (%)')
-    for step in range(len(df)):
-        display_input_widgets(df, step)
+    
+    if uploaded_file is None:
+        for step in range(ticker_count):
+            display_input_widgets2(step)
+    
+    if ticker_count = 0:
+        for step in range(len(df)):
+            display_input_widgets(df, step)
     submitted = form.form_submit_button("Submit")
     
     
