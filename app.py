4d92b9c1f7096b61f35a68327e83541a4d41d71e
thruway22 <121792615+thruway22@users.noreply.github.com>
2023-02-01 07:08:45 +0300

    Update app.py

--- app.py
+++ app.py
@@ -22,38 +22,36 @@ csv_file = st.file_uploader('upload a file', type='CSV')
 if csv_file is None:
     #with st.expander('or input manually'):
     ticker_count = st.number_input('or choose number of stocks to input manually', value=0, min_value=0)
-    items_length = ticker_count
+
+    
+    
+
+if csv_file is not None or ticker_count > 0:
     form = st.form('manual_ticker_form')
     cola, colb, colc = form.columns(3)
     cola.write('Ticker')
     colb.write('Current Shares (x)')
     colc.write('Target Weight (%)')
-    for step in range(ticker_count):
-        display_input_widgets2(step)
-    submitted = form.form_submit_button("Submit")
-    
-else:
-    df = pd.read_csv(csv_file, names=['ticker', 'current_shares', 'target_weight'])
-    df = df.set_index('ticker')
-    items_length = len(df)
-    form = st.form('read_ticker_form')
-    cola, colb, colc = form.columns(3)
-    cola.write('Ticker')
-    colb.write('Current Shares (x)')
-    colc.write('Target Weight (%)')
-    for step in range(len(df)):
+    if csv_file is not None:
+        df = pd.read_csv(csv_file, names=['ticker', 'current_shares', 'target_weight'])
+        df = df.set_index('ticker')
+        items_length = len(df)
+        for step in range(len(df)):
             display_input_widgets(df, step)
+    if ticker_count > 0:
+        for step in range(ticker_count):
+            display_input_widgets2(step)    
     submitted = form.form_submit_button("Submit")
 
-if submitted or csv_file is not None:
-    sum_target = 0
-    for step in range(items_length):
-        sum_target += globals()['target%s' % step]
-    
-    if sum_target == 100:
-        st.success('Setting target weights successful!')
-    else:
-        st.error('Sum of target weights must equal 100%')
+    if submitted:
+        sum_target = 0
+        for step in range(items_length):
+            sum_target += globals()['target%s' % step]
+
+        if sum_target == 100:
+            st.success('Setting target weights successful!')
+        else:
+            st.error('Sum of target weights must equal 100%')
    
     
     
