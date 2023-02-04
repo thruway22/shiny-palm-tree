b75a509d44e2196327778fbd9b78f170cb3788c8
thruway22 <121792615+thruway22@users.noreply.github.com>
2023-02-04 10:23:21 +0300

    u

--- app.py
+++ app.py
@@ -149,8 +149,10 @@ if submitted:
         st.write(account_cash)
         
         algo_list = []
-        for i in df.index:
-            value = (total_cash + df['market_value'].sum()) * (df['target_weight'][i]/100) - df['market_value'][i]
+        for i in range(len(df)): 
+            value = (total_cash + df['market_value'].sum()) * (
+                df.iat[i, df.columns.get_loc('target_weight')]/100
+                ) - df.iat[i, df.columns.get_loc('market_value')]
             value = value if allow_selling else max(value, 0)
             algo_list.append(value)
         df['algo'] = algo_list
