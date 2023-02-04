7b6d12eee723807df226115c3ff38d11fa45deff
thruway22 <121792615+thruway22@users.noreply.github.com>
2023-02-04 11:00:19 +0300

    u

--- app.py
+++ app.py
@@ -97,7 +97,7 @@ if submitted:
         sum_target += target
         ticker_list.append(ticker)
 
-    prohibit_duplicates = False    
+    prohibit_duplicates = True    
     if prohibit_duplicates and len(ticker_list) != len(set(ticker_list)):
         st.error('Duplicates are not allowed')
         st.stop()
