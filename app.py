f1fe2d301a5cb5be52756ca1588353b2ad5530d8
thruway22 <121792615+thruway22@users.noreply.github.com>
2023-02-05 17:12:03 +0300

    u

--- app.py
+++ app.py
@@ -199,14 +199,16 @@ if submitted:
 
         st.write(inward_dict, outward_dict)
 
+
+
         fig = go.Figure(data=[go.Sankey(
-            node = dict(
-            pad = 15,
-            thickness = 20,
-            line = dict(color = "black", width = 0.5),
-            label = ["A1", "A2", "B1", "B2", "C1", "C2"],
-            color = "blue"
-            ),
+            # node = dict(
+            # pad = 15,
+            # thickness = 20,
+            # line = dict(color = "black", width = 0.5),
+            # label = ["A1", "A2", "B1", "B2", "C1", "C2"],
+            # color = "blue"
+            # ),
             link = dict(
             source = [0, 1, 0, 2, 3, 3], # indices correspond to labels, eg A1, A2, A1, B1, ...
             target = [2, 3, 3, 4, 4, 5],
