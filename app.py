f6a8e09ba5ca47aa1e2ea1fde8c68c289e76766f
thruway22 <121792615+thruway22@users.noreply.github.com>
2023-02-05 12:48:43 +0300

    u

--- app.py
+++ app.py
@@ -197,7 +197,7 @@ if submitted:
             }
 
             st.plotly_chart(
-                px.icicle(cash_handling_dict2, parents='level_a', names='level_b', values='values').update_traces(root_color="lightgrey")
+                px.icicle(cash_handling_dict2, parents='level_a', names='level_b', values='values', color='day')#.update_traces(root_color="lightgrey")
             )
 
             st.write(
