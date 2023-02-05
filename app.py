3cd96682fb624bbc2f51630e90abfbc706cf4a66
thruway22 <121792615+thruway22@users.noreply.github.com>
2023-02-05 12:03:29 +0300

    u

--- app.py
+++ app.py
@@ -180,7 +180,7 @@ if submitted:
         df['post_trade_weight'] = 100 * (df['market_value'] + df['output_value']) / (df['market_value'].sum() + df['output_value'].sum())
 
         if allow_fractional == False:
-            st.write('Your total available cash to trade is', ':.2f'.format(total_cash))
+            st.write('Your total available cash to trade is', '{}:.2f'.format(total_cash))
 
         # st.header('Plan:')
 
