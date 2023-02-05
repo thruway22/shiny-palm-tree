a00b79545958d13018669f59a519801b36e53bb0
thruway22 <121792615+thruway22@users.noreply.github.com>
2023-02-05 12:21:09 +0300

    u

--- app.py
+++ app.py
@@ -180,7 +180,11 @@ if submitted:
         df['post_trade_weight'] = 100 * (df['market_value'] + df['output_value']) / (df['market_value'].sum() + df['output_value'].sum())
 
         if allow_fractional == False:
-            st.write('Your total available cash to trade is', '**${:.2f}**'.format(total_cash))
+            st.write(
+                '''Your total available cash to trade is **${:.2f}**,
+                but you can only trade **{:.2f}** for complete (non-fractional) shares.
+                With excess cash of **{:.2f}**, how would you like to distribute it?'''.format(total_cash, total_cash - excess_cash, excess_cash)
+            )
 
         # st.header('Plan:')
 
