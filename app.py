b0d2d023e2a28925ff5733c68a3b9e2efba99af4
thruway22 <121792615+thruway22@users.noreply.github.com>
2023-02-12 03:40:54 +0300

    u

--- app.py
+++ app.py
@@ -220,6 +220,9 @@ if csv_file is not None or widgets_length > 0:
                 #     for k in account_cash_dict_aux.keys():
                 #         account_cash_dict_aux[k] = [account_cash] if k == important_currency else [0]
                 excess_cash_weighted = {k: excess_cash * (sum(v)/account_cash) for (k, v) in account_cash_dict.items()}
+                if important_currency_checkbox:
+                    for k in excess_cash_weighted.keys():
+                        excess_cash_weighted[k] = [excess_cash] if k == important_currency else [0]
                 df['output_value'] = 0
                 for i in df.index:
                     if i not in account_cash_dict.keys(): # for tickers
