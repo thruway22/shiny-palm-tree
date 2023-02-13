e25f9a7af9eeb4ea01136f9c777220384d5fa15c
thruway22 <121792615+thruway22@users.noreply.github.com>
2023-02-14 01:58:03 +0300

    u

--- app.py
+++ app.py
@@ -351,7 +351,7 @@ if csv_file is not None or widgets_length > 0:
                 ticker = yf.Ticker(i)
                 account_list.append(ticker.fast_info['currency'])
             df['account'] = account_list
-            pie_fig = px.sunburst(df.reset_index(), path=['ticker', ], values='post_trade_weight', color='account')
+            pie_fig = px.sunburst(df.reset_index(), path='ticker', values='post_trade_weight', color='account')
             st.plotly_chart(pie_fig, use_container_width=True, config= {'displayModeBar': False})   
                 
     
