8c962af392d8bef978633eaf8961c29daf0b07da
thruway22 <121792615+thruway22@users.noreply.github.com>
2023-02-14 02:21:46 +0300

    u

--- app.py
+++ app.py
@@ -355,11 +355,8 @@ if csv_file is not None or widgets_length > 0:
                     ticker = yf.Ticker(i)
                     account_list.append(ticker.fast_info['currency'])
             df['account'] = account_list
-            left, middle, right = st.columns(3)
+            left, right = st.columns(2)
             left.plotly_chart(
-                px.sunburst(df.reset_index(), path=['account', 'ticker'], values='pre_trade_weight', color='account'),
-                use_container_width=True, config= {'displayModeBar': False})
-            middle.plotly_chart(
                 px.sunburst(df.reset_index(), path=['account', 'ticker'], values='target_weight', color='account'),
                 use_container_width=True, config= {'displayModeBar': False})
             right.plotly_chart(
