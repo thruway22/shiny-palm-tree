fb4899a7b6dd81cb6e4ac1ff01db545043772599
thruway22 <121792615+thruway22@users.noreply.github.com>
2023-02-17 01:58:52 +0300

    u

--- app.py
+++ app.py
@@ -254,7 +254,7 @@ if csv_file is not None or widgets_length > 0:
             values_list = [contribution_cash]
             sources_list = [0]
             targets_list = []
-            cash_df = pd.DataFrame({'Item': labels_list, 'Value': values_list})
+            cash_df = pd.DataFrame({'Item': labels_list, 'Value': values_list}).set_index('Item')
 
             sources_length = len(sources_list) - 1 # left-section: account cash
             for k, v in account_cash_dict.items():
@@ -262,6 +262,7 @@ if csv_file is not None or widgets_length > 0:
                 values_list.append(abs(sum(v)))
                 sources_length += 1
                 sources_list.append(sources_length)
+                #cash_df
 
             sources_length = len(sources_list) - 1 # left-section: selling
             for i in df[df['output_value'] < 0].index:
@@ -316,7 +317,7 @@ if csv_file is not None or widgets_length > 0:
 
             st.header('Plan:')
 
-            st.table(cash_df.style.hide_index())
+            st.table(cash_df)
 
             plan_df = df[df['output_value'] != 0][['price', 'output_unit', 'output_value']].sort_values('output_value').reset_index().rename(columns={
                 'ticker': 'Ticker',
