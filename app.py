2331a6a5b14dc93bce8c592b5b9131d62f304f75
thruway22 <121792615+thruway22@users.noreply.github.com>
2023-02-19 03:26:04 +0300

    u

--- app.py
+++ app.py
@@ -306,29 +306,48 @@ if csv_file is not None or widgets_length > 0:
             flow_fig.update_layout(title_text='Cash Flow Diagram')
             flow_fig.update_xaxes(fixedrange=True)
             flow_fig.update_yaxes(fixedrange=True)
-            st.plotly_chart(flow_fig, use_container_width=True, config= {'displayModeBar': False})
 
-            st.header('Detailed Transactions Flow')
 
-            st.subheader('Inflows:')
+            # output = f'''<div id="metric_block">
+            #       <p id="metric_value">{metric1:.2f}{fmt_dict[metric1_fmt]}</p>
+            #       <p id="metric_label">{texts.loc[metric1_label].value}</p>
+            #       <p id="metric_value">{metric2:.2f}{fmt_dict[metric2_fmt]}</p>
+            #       <p id="metric_label">{texts.loc[metric2_label].value}</p>
+            #       <p id="metric_value">{metric3:.2f}{fmt_dict[metric3_fmt]}</p>
+            #       <p id="metric_label">{texts.loc[metric3_label].value}</p>
+            #       </div>'''
+            # return st.markdown(output, unsafe_allow_html=True)
+
             inflow_cash = total_cash + df[df['output_value'] < 0]['output_value'].abs().sum()
+
+            metrics_block = '''<div id="metric_block">
+                            <p id="metric_label">Available Cash</p>
+                            <p id="metric_value">${:.2f}</p>
+                            <p id="metric_label">Tradable Cash</p>
+                            <p id="metric_value">${:.2f}</p>
+                            <p id="metric_label">Excess Cash</p>
+                            <p id="metric_value">${:.2f}</p>
+                            </div>'''.format(inflow_cash, inflow_cash - excess_cash, excess_cash)
+
+            st.markdown(metrics_block, unsafe_allow_html=True)
+            st.plotly_chart(flow_fig, use_container_width=True, config= {'displayModeBar': False})
 
             cash_df.loc['Total'] = [cash_df['Amount ($)'].sum()]
             cash_df = cash_df.reset_index()
             cash_df.index += 1
             st.table(cash_df.style.format(precision=2, na_rep='', thousands=','))
 
-            output_text = 'Your total available cash to trade is **\${:.2f}**'.format(inflow_cash)
-            if allow_fractional == False:
-                output_text += ', but you can only trade **\${:.2f}** for complete (non-fractional) shares with excess cash of **\${:.2f}**.'.format(inflow_cash - excess_cash, excess_cash)
-            else:
-                output_text += '.'
-            st.write(output_text)
+            # output_text = 'Your total available cash to trade is **\${:.2f}**'.format(inflow_cash)
+            # if allow_fractional == False:
+            #     output_text += ', but you can only trade **\${:.2f}** for complete (non-fractional) shares with excess cash of **\${:.2f}**.'.format(inflow_cash - excess_cash, excess_cash)
+            # else:
+            #     output_text += '.'
+            # st.write(output_text)
 
-            left, middle, right = st.columns(3)
-            left.metric(label='Available Cash', value='${:.2f}'.format(inflow_cash))
-            middle.metric(label='Tradable Cash', value='${:.2f}'.format(inflow_cash - excess_cash))
-            right.metric(label='Excess Cash', value='${:.2f}'.format(excess_cash))
+            # left, middle, right = st.columns(3)
+            # left.metric(label='Available Cash', value='${:.2f}'.format(inflow_cash))
+            # middle.metric(label='Tradable Cash', value='${:.2f}'.format(inflow_cash - excess_cash))
+            # right.metric(label='Excess Cash', value='${:.2f}'.format(excess_cash))
 
             # if allow_fractional == False:
             #     inflow_cash = total_cash + df[df['output_value'] < 0]['output_value'].abs().sum()
